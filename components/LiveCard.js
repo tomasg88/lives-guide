e652c5b82c4be4fb3f2660c56ce11a6c4e1f6532
Tomas <tomasg88@gmail.com>
2020-12-09 20:24:47 -0300

    tw y sc

--- components/LiveCard.js
+++ components/LiveCard.js
@@ -1,48 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import tw from "tailwind-styled-components";
 
 const CardContainer = tw.div`
-	relative flex items-end justify-center p-5 rounded-xl bg-white shadow-lg min-w-40 max-w-md h-64 mx-5 z-10 hover:shadow-xl hover:transition-shadow transition-shadow
+	relative flex items-end justify-center 
+	min-w-40 max-w-md h-80
+	mx-5 p-5 z-10 
+	rounded-xl shadow-lg bg-transparent
+	hover:shadow-xl hover:transition-shadow transition-shadow
+`
+
+const ImageContainer = tw.div`
+	absolute block h-full w-full z-0 top-0 left-0
 `
 
 const DataContainer = tw.div`
-	flex flex-col p-2 w-full h-1/6 relative bg-white rounded-xl
+	flex flex-col p-2 w-4/6 max-h-1/2 relative bg-white rounded-xl
+`
+const GoButton = tw.button`
+	bg-gray-400 px-10 py-1 rounded-full shadow-md hover:bg-gray-700 hover:text-white transition-colors
 `
 
-const LiveCard = ({card}) => {
+const LiveCard = ({ card }) => {
+	const [isExpanded, setIsExpanded] = useState(false)
+	const handleClick = () => {
+		setIsExpanded(!isExpanded)
+	}
 	return (
 		<CardContainer>
-			<div className="absolute block h-full w-full z-0 top-0 left-0">
-				{/* <div style={styling}></div> */}
+			<ImageContainer>
 				<Image
 					src={"/samples/Desert.jpg"}
 					alt="Show picture"
 					layout="fill"
 					className="w-full h-full rounded-xl"
 				/>
-			</div>
+				
+			</ImageContainer>
 			<DataContainer>
 				<h3 className="text-bold text-xl text-center">
 					{card.title}
 				</h3>
-				<p className="text-thin text-xs">
-					{card.description ||
-						"Aca una descripcion de lo que que se que va a hablar en el show"}
-				</p>
-				<div className="rrss">
-					<span className="icon"></span>
-					<div className="text-bold">
-						{card.account || "_tomygonzalez.dev"}
-					</div>
-				</div>
-				<p className="text-gray-700">
-					{card.when}
-				</p>
+				{
+					isExpanded && (
+						<>
+							<p className="text-thin text-xs">
+								{card.description}
+							</p>
+							<div className="rrss">
+								<span className="icon"></span>
+								<div className="text-bold">
+									{card.account}
+								</div>
+							</div>
+							<p className="text-gray-700">
+								{card.when}
+							</p>
+						</>
+					)
+				}
 				<div className="self-center h-12 flex items-center">
-					<button className="bg-gray-400 px-10 py-1 rounded-full shadow-md hover:bg-gray-700 hover:text-white transition-colors">
+					<GoButton onClick={handleClick}>
 						Ir
-					</button>
+					</GoButton>
 				</div>
 			</DataContainer>
 		</CardContainer>
